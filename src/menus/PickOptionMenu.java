a71f302abbc861b46ce55404ce969fc982ae843d
d-rocham <d_rocham@outlook.com>
2022-07-16 09:19:20 -0500

    Changed type of menuOptions from List to HashMap

--- src/menus/PickOptionMenu.java
+++ src/menus/PickOptionMenu.java
@@ -1,14 +1,22 @@
 package menus;
 
+import java.util.HashMap;
 import java.util.List;
 
 public abstract class PickOptionMenu {
-    private List<String> menuOptions;
+
+    private HashMap<Integer, String> menuOptions;
     private String menuTitle;
 
     protected PickOptionMenu(List<String> menuOptions, String menuTitle) {
-        this.menuOptions = menuOptions;
+        setMenuOptions(menuOptions);
         this.menuTitle = menuTitle;
+    }
+
+    public void setMenuOptions(List<String> menuOptionsList) {
+        for (int idx = 0; idx < menuOptionsList.size(); idx++) {
+            this.menuOptions.put(idx + 1, menuOptionsList.get(idx));
+        }
     }
 
     protected abstract int menuAction();
